485e85b07d06ebfa11f1d7693e78d5d578267f36
EtienneN10 <etienne10.ngolo@gmail.com>
2021-05-14 22:48:30 +0000

    Ajout des codes de Jordan

---
+++ codes_jo/premier_league.php
@@ -0,0 +1,33 @@
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="../css/common_style.css">
+        <title>Premier League</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul id="menu">
+                    <li><a href="ligue_1.php">Ligue 1</a></li>
+                    <li><a href="premier_league.php">Premier League</a></li>
+                    <li><a href="laliga.php">La Liga</a></li>
+                    <li><a href="bundesliga.php">Bundesliga</a></li>
+                    <li><a href="serie_a.php">Serie A</a></li>
+                </ul>
+            </nav>
+        </header>
+        <h1>Premer League</h1>
+        <section>
+            <p>Matchs à venir :</p>
+            <table>
+                <tr></tr>
+            </table>
+        </section>
+        <section>
+            <p>Classement :</p>
+            <table>
+                <tr></tr>
+            </table>
+        </section>
+    </body>
+</html>
